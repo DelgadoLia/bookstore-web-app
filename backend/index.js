9984d5b1640688a20dc5a3e6698d2dccb20dad03
Elia Arteaga Delgado <delgadolia76@gmail.com>
2026-04-12 10:59:33 -0600

    Adicion de API productos en backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app=express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({origin:'http://localhost:4200'}));
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+const productoRutas = require('./routes/db.routes');
+app.use('/api/productos', productoRutas);
+
+app.get('/', (req, res) => {
+    res.json({mensaje: 'Servidor funcionando'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+});
