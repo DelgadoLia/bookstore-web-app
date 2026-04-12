2225ddc88bae255a6c70a49a5fbfb6545ebc18a2
Elia Arteaga Delgado <delgadolia76@gmail.com>
2026-04-12 11:25:33 -0600

    Adicion de API al frontend

---
+++ bookstore-web-app/src/app/core/models/producto.model.ts
@@ -0,0 +1,12 @@
+export interface Producto {
+  id: number;
+  nombre: string;
+  categoria: string;
+  editorial: string;
+  tomo: number;
+  precio: number;
+  stock: number;
+  imagen: string;
+  descripcion: string;
+  disponible: number; 
+}
