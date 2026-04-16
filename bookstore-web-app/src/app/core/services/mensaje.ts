75f23f17241329773cef367fc9cec7968630410c
Elia Arteaga Delgado <delgadolia76@gmail.com>
2026-04-15 20:16:40 -0600

    otro error

--- bookstore-web-app/src/app/core/services/mensaje.ts
+++ bookstore-web-app/src/app/core/services/mensaje.ts
@@ -4,14 +4,14 @@ import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Mensaje } from '../models/mensaje.model';
 
+
 @Injectable({ providedIn: 'root' })
 export class MensajeService {
   private http = inject(HttpClient);
   private apiUrl = 'https://backend-bookstore-yzkx.onrender.com/api/mensajes';
-
   private cache: Mensaje[] = [];
 
-  getMensajes(): Observable<Mensaje[]> {
+  getMensajes(): Observable<Mensaje[]>  {
     if (this.cache.length > 0) {
       return of(this.cache);
     }
