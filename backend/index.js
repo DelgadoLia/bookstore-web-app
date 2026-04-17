eb5eecdcd2a46d3fa296fa93547843c7fd28979b
Elia Arteaga Delgado <delgadolia76@gmail.com>
2026-04-17 11:59:13 -0600

    error en ruta

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,25 @@ app.use('/api/productos', productoRutas);
 const mensajesRoutes = require('./routes/mensaje.routes');
 app.use('/api/mensajes', mensajesRoutes);
 
+app.get('/api/manga', async (req, res) => {
+  try {
+    const { title, limit } = req.query;
+
+    let url = `https://api.mangadex.org/manga?includes[]=cover_art&order[followedCount]=desc`;
+    
+
+    if (limit) url += `&limit=${limit}`;
+    if (title) url += `&title=${title}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener mangas' });
+  }
+});
+
 app.get('/', (req, res) => {
     res.json({mensaje: 'Servidor funcionando'});
 });
